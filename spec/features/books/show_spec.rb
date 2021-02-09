57a6508ffd8b61946b08c13967ba106d8ed3144c
Diana Buffone <diana.pamelaet@gmail.com>
2021-02-09 18:57:27 -0500

    Pass test user story 10 for books

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -12,5 +12,18 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_content(book2.title)
       expect(page).to have_content(book2.copies_available)
     end
+
+    it 'has link to delete the book' do
+      little_creek = Library.create!(name: 'Little Creek', current_employees: 20)
+      book2 = little_creek.books.create!(title: 'Coding for Dummies', copies_available: 20)
+
+      visit "/books/#{book2.id}"
+
+      click_link "Delete Book"
+
+      expect(current_path).to eq('/books')
+      expect(page).to_not have_content(book2.title)
+      expect(page).to_not have_content(book2.copies_available)
+    end
   end
 end
