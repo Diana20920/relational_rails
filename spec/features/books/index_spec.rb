7997328de735aecc931ab1f1d9c4069a258323b3
Diana Buffone <diana.pamelaet@gmail.com>
2021-02-09 17:48:01 -0500

    Pass test for user story 7 books and characters

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -15,5 +15,28 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_content(book2.title)
       expect(page).to have_content(book2.copies_available)
     end
+
+  describe "I visit the library's books page" do
+    it 'shows all the books for that library' do
+      little_creek = Library.create!(name: 'Little Creek', current_employees: 20)
+      hogwarts = Library.create!(name: 'Hogwarts', current_employees: 3)
+
+      book1 = little_creek.books.create!(title: 'Ruby', copies_available: 3)
+      book2 = little_creek.books.create!(title: 'Coding for Dummies', copies_available: 20)
+
+      book3 = hogwarts.books.create!(title: 'Magic for Muggles', copies_available: 2)
+      book4 = hogwarts.books.create!(title: 'Advanced Spells', copies_available: 6)
+
+      visit "/libraries/#{little_creek.id}/books"
+
+      expect(page).to have_content(book1.title)
+      expect(page).to have_content(book1.copies_available)
+      expect(page).to have_content(little_creek.id)
+      expect(page).to have_content(book2.title)
+      expect(page).to have_content(book2.copies_available)
+      expect(page).to_not have_content(book3.title)
+      expect(page).to_not have_content(book4.copies_available)
+      end
+    end
   end
 end
--- spec/features/characters/index_spec.rb
+++ spec/features/characters/index_spec.rb
@@ -16,5 +16,28 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_content(char2.name)
       expect(page).to have_content(char2.age)
     end
+
+    describe "I visit the novel's characters page" do
+      it 'shows all the characters for that novel' do
+        eleven_minutes = Novel.create!(title: 'Eleven Minutes', author: 'Paulo Coelho', number_of_chapters: 10)
+        hyos = Novel.create!(title: '100 Years of Solitude', author: 'Gabriel García Márquez', number_of_chapters: 30)
+
+        char1 = eleven_minutes.characters.create!(name: 'Ralf Hart', age: 38)
+        char2 = eleven_minutes.characters.create!(name: 'Nyah', age: 27)
+
+        char3 = hyos.characters.create!(name: 'Amaranta', age: 20)
+        char4 = hyos.characters.create!(name: 'Úrsula Iguarán', age: 45)
+
+        visit "/novels/#{hyos.id}/characters"
+
+        expect(page).to have_content(char3.name)
+        expect(page).to have_content(char3.age)
+        expect(page).to have_content(hyos.id)
+        expect(page).to have_content(char4.name)
+        expect(page).to have_content(char4.age)
+        expect(page).to_not have_content(char1.name)
+        expect(page).to_not have_content(char2.age)
+      end
+    end
   end
 end
