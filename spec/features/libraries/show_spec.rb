524cc2a38fdf795e84852914b053dd7c4b74ee39
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-09 09:34:18 -0600

    Add feature test for user story 5 for Library.

--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(little_creek.current_employees)
       expect(page).to have_content(little_creek.online_membership)
     end
+
+    it "then has link to delete library" do
+      little_creek = Library.create!(name: 'Little Creek', current_employees: 20, online_membership: true)
+
+      visit "/libraries/#{little_creek.id}"
+
+      click_link 'Delete Library'
+
+      expect(current_path).to eq("/libraries")
+      expect(page).to_not have_content(little_creek.name)
+    end
   end
 end
