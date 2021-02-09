6812b34a5995b45bf2c21e0548ecfbfd6e36d017
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-08 20:22:01 -0600

    Write test for user story 1 for Library.

---
+++ spec/features/libraries/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  describe 'I visit a libraries index page' do
+    it 'shows name of each library' do
+      library1 = Library.create!(name: 'Library 1', current_employees: 5, online_membership: true)
+      little_creek = Library.create!(name: 'Little Creek', current_employees: 20, online_membership: true)
+      hogwarts = Library.create!(name: 'Hogwarts', current_employees: 3, online_membership: true)
+
+      visit "/libraries"
+
+      expect(page).to have_content(library1.name)
+      expect(page).to have_content(little_creek.name)
+      expect(page).to have_content(hogwarts.name)
+    end
+  end
+end
