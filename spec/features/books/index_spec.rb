d3e3134bf04b26c2cb381005d5437ff376a15fd0
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-09 21:36:37 -0600

    Add test for user story 14 for Book.

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -15,6 +15,20 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_content(book2.title)
       expect(page).to have_content(book2.copies_available)
     end
+
+    it "displays the records with audio_book as true" do
+      hogwarts = Library.create!(name: 'Hogwarts', current_employees: 3)
+      book1 = hogwarts.books.create!(title: 'Magic for Muggles', copies_available: 2, audio_book: true)
+      book2 = hogwarts.books.create!(title: 'Advanced Spells', copies_available: 6, audio_book: true)
+      book3 = hogwarts.books.create!(title: 'Negative Spells', copies_available: 8, audio_book: false)
+
+      visit "/books"
+
+      expect(page).to have_content(book1.title)
+      expect(page).to have_content(book2.title)
+      expect(page).to_not have_content(book3.title)
+    end
+
   end
 
   describe "I visit the library's books page" do
